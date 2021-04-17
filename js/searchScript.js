f5b73d80dc05a194125110dee7a229f4cf2b61ce
Gustavo Cauzzi <gustavocauzzi@gmail.com>
2021-04-17 00:50:23 -0300

    🚀 Funcionalidades da tela de pesquisa terminadas

--- js/searchScript.js
+++ js/searchScript.js
@@ -1,15 +1,23 @@
-function search(){
-  const value = document.getElementById("txtSearch").value;
-  window.AppInventor.setWebViewString(value);  
+function search(data){
+  const value = data ? data : document.getElementById("txtSearch").value;
+
+  if (value === '') return; // Se deu true aqui, o usuário n clicou em nenhum botão e n dgitou nada, então n é pra fazer nada
+
+  window.AppInventor.setWebViewString(JSON.stringify({
+    isValid: true,
+    value,
+    goToScreen: 'html2' // Alterar para o nome da página no app final
+  }));  
 }
 
-function generateList(historyArray){ // Array com todos os históricos
+function generateList(historyArray){ // Array com todo o histórico de pesquisa já formatado (formatar no app inventor)
   historyArray.map(historyItem => {
-    let list = document.getElementById('historyList').innerHTML;
-
     document.getElementById('historyList').innerHTML += 
-    `<li class="listItem ${list ? 'withBorder' : ''}">
-    ${historyItem}
+    `<li
+      class="listItem"
+      onClick="search('${historyItem}');"
+    >
+      ${historyItem}
     </li>`;
   })
 }
