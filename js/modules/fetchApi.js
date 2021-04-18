20c2dd41411ba302f4c448705145efa042289e3d
Gustavo Cauzzi <gustavocauzzi@gmail.com>
2021-04-18 17:17:36 -0300

    Estilização da página Home

--- js/modules/fetchApi.js
+++ js/modules/fetchApi.js
@@ -1,15 +1,31 @@
-export default function fetchApi(url, targetConfirmados, targetRecuperados, targetMortos)
+function fetchApi(url, targetConfirmados, targetRecuperados, targetMortos, targetAtivos)
 {
   fetch(url)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
+    const {confirmed, recovered, deaths, population} = data.All;
+
     const numerosConfirmados = document.querySelector(targetConfirmados);
     const numerosRecuperados = document.querySelector(targetRecuperados);
     const numerosMortes = document.querySelector(targetMortos);
-    numerosConfirmados.innerHTML = data.All.confirmed;
-    numerosRecuperados.innerHTML = data.All.recovered ;
-    numerosMortes.innerHTML = data.All.deaths;
+    const numerosAtivos = document.querySelector(targetAtivos);
+
+    const ativos = confirmed - deaths - recovered;
+
+    numerosConfirmados.innerHTML = confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    numerosRecuperados.innerHTML = recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    numerosMortes.innerHTML = deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    numerosAtivos.innerHTML = ativos.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    const indiceBandeira = ativos * 100 / population;
+    if (indiceBandeira > 0.5) {
+      console.log("vermelho")
+    } else if (indiceBandeira <= 0.5 && indiceBandeira > 2.3){
+      console.log("amarelo")
+    } else {
+      console.log("verde")
+    }
   });
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
-import fetchApi from './modules/fetchApi.js';
+function fetchApi(url, targetConfirmados, targetRecuperados, targetMortos, targetAtivos)
+{
+  fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const {confirmed, recovered, deaths, population} = data.All;
 
-fetchApi('https://covid-api.mmediagroup.fr/v1/cases?country=Brazil', '.numerosConfirmados', '.numerosRecuperados', '.numerosMortes');
+    const numerosConfirmados = document.querySelector(targetConfirmados);
+    const numerosRecuperados = document.querySelector(targetRecuperados);
+    const numerosMortes = document.querySelector(targetMortos);
+    const numerosAtivos = document.querySelector(targetAtivos);
+
+    const ativos = confirmed - deaths - recovered;
+
+    numerosConfirmados.innerHTML = confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    numerosRecuperados.innerHTML = recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    numerosMortes.innerHTML = deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    numerosAtivos.innerHTML = ativos.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    const indiceBandeira = ativos * 100 / population;
+    if (indiceBandeira > 0.5) {
+      console.log("vermelho")
+    } else if (indiceBandeira <= 0.5 && indiceBandeira > 2.3){
+      console.log("amarelo")
+    } else {
+      console.log("verde")
+    }
+  });
+}
+
+fetchApi('https://covid-api.mmediagroup.fr/v1/cases?country=Brazil', '.numerosConfirmados', '.numerosRecuperados', '.numerosMortes', '.numerosAtivos');
