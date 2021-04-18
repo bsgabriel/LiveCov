820290a25370fbd164eeefdcf009c07f4d836b52
Gustavo Cauzzi <gustavocauzzi@gmail.com>
2021-04-17 23:41:38 -0300

    Botão de voltar adicionado para a tela de pesquisa

--- js/searchScript.js
+++ js/searchScript.js
@@ -1,23 +1,48 @@
+/*
+  isValid: Colocar valid: true para caso o .WebViewStringChange seja
+  chamado quando a gente não quiser, não rodar nada.
+  Lembrar de colocar um teste para testar se o isValid está como true
+  lá no app inventor antes de executar
+
+  action = o que é pra fazer lá no app inventor
+  - tipos:
+    "search" = Vai mandar um valor "value" para ser pesquisado na api em outra página
+    "goBack" = Voltar para a página anterior
+*/
+
 function search(data){
   const value = data ? data : document.getElementById("txtSearch").value;
 
-  if (value === '') return; // Se deu true aqui, o usuário n clicou em nenhum botão e n dgitou nada, então n é pra fazer nada
+  if (value === '') return; // Se deu true aqui, o usuário n clicou em nenhum botão e n digitou nada, então n é pra fazer nada
 
   window.AppInventor.setWebViewString(JSON.stringify({
     isValid: true,
-    value,
-    goToScreen: 'html2' // Alterar para o nome da página no app final
+    action: 'search',   
+    value,              
+    goToScreen: 'html2',
   }));  
 }
 
-function generateList(historyArray){ // Array com todo o histórico de pesquisa já formatado (formatar no app inventor)
-  historyArray.map(historyItem => {
+function generateList(historyArray){ 
+  // historyArray =  Array com todo o histórico de pesquisa já formatado do mais antigo para o mais atual (formatar no app inventor)
+  historyArray.reverse().map(historyItem => {
     document.getElementById('historyList').innerHTML += 
     `<li
       class="listItem"
       onClick="search('${historyItem}');"
     >
-      ${historyItem}
+      <div class="itemTextContainer">
+        ${historyItem}
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" class="listItemSvg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
     </li>`;
   })
+}
+
+function goBack(){
+  window.AppInventor.setWebViewString(JSON.stringify({
+    isValid: true,        
+    action: 'goBack',     // Linha 1                
+    goToScreen: 'Screen1' 
+  }));  
 }
